61a54138bf985d210cd4f5d67625b62652f31e96
hartator <hartator@gmail.com>
2015-09-14 10:54:58 -0500

    Compute score for stories

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -7,12 +7,16 @@ class Algorithm
   end
 
   def score_stories
+    gravity = 1.8
     stories = retrieve_stories[:hits]
-    stories[0..3].map do |story|
-      story[:hours_ago] = (Time.zone.now - Time.zone.parse(story[:created_at])) / 1.hour
-      story[:score] = 
-      binding.pry
+    stories.map! do |story|
+      story[:points] = story[:points].to_f
+      story[:created_at] = Time.zone.parse(story[:created_at])
+      story[:hours_ago] = (Time.zone.now - story[:created_at]) / 1.hour
+      story[:score] = (story[:points]-1) ** 0.8 / (story[:hours_ago] + 2) ** 1.8
+      story
     end
+    stories.sort_by {|e| e[:score]}
   end
 
 end
